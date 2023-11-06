eb9fcfe574c7b4318d6a7a53fe6e95db87509476
OmarElkeshen38 <oelkeshen5@gmail.com>
2023-11-06 05:36:15 +0200

    shit

--- src/components/CustomersComp/CustomersComp.jsx
+++ src/components/CustomersComp/CustomersComp.jsx
@@ -1,37 +1,43 @@
 import React from 'react';
-import Styles from './CustomersComp.module.css'
+import Styles from './CustomersComp.module.css';
+import customerImg from '../../assets/customer.jpg'
 function CustomersComp() {
   return (
     <>
       <div className={Styles.customers}>
         <h2>Customers List</h2>
 
-        <table className="table">
+        <table className="bg-dange">
           <thead>
             <tr>
-              <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
+              <th>
+                <h2>Customer</h2>
+              </th>
+              <th>
+                <h2>Order</h2>
+              </th>
+              <th>
+                <h2>Total</h2>
+              </th>
+              <th>
+                <h3>chat</h3>
+              </th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
+              <td className='bg-danger'>
+                
+              </td>
+              <td>4 items</td>
+              <td>12,00$</td>
+              <td>12,00$</td>
             </tr>
             <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td colspan="2">Larry the Bird</td>
-              <td>@twitter</td>
+              <td>Ahmed Mohamed</td>
+              <td>4 items</td>
+              <td>12,00$</td>
+              <td>12,00$</td>
             </tr>
           </tbody>
         </table>
